caa0aac86f4ecd28f18903d887f2480253f96f96
Nguyen Huu Phuc <huuphuc.nguyen@nfq.asia>
2021-01-19 15:14:59 +0700

    init simple

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# test
+# test [![Coverage Status](https://coveralls.io/repos/github/phucnh-nfq/test/badge.svg?branch=main)](https://coveralls.io/github/phucnh-nfq/test?branch=main)
 Just for test
---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SimpleTest extends WebTestCase
+{
+    public function testSimple()
+    {
+        $this->assertEquals(200, 400 / 2);
+    }
+}
